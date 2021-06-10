928e064f2cb037bd22addd9dc17e7b2ec0faa6b4
jashimgit <jashim.ncn@gmail.com>
2021-06-11 00:46:00 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from './images/memories.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography align="center" variant="h2">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow>
+        <Container>
+          <Grid container justify='space-between' alignItems="stretch"   >
+            <Grid item={12} sm={7}>
+              <Typography varient="h2">Post</Typography>
+            </Grid>
+            <Grid  item={12} sm={7}>
+              <Typography varient="h2">Form</Typography>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
